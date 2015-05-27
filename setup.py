5ccfba077ab507cbcc4a6b1c10e5f1085a4c4c87
James Boocock <james.boocock@otago.ac.nz>
2015-05-27 14:49:47 +1200

    Pyrallel first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyrallel",
+    version="1.0",
+    packages=['pyrallel'],
+    author="James Boocock",
+    author_email="james.boocock@otago.ac.nz",
+    description="Simple interface to create parallel scripts using python",
+    license="MIT",
+    zip_safe=False,
+    url="github.com/smilefreak/pyrallel",
+    use_2to3=True,
+)
